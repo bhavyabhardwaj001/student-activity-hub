b49ef5f7ec355c3a8c27fc486b6b94fd78ffaa43
bhavyabhardwaj001 <161822108+bhavyabhardwaj001@users.noreply.github.com>
2026-02-18 00:56:00 +0530

    Connect Backend to MongoDB atlas

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed:", error);
+  });
 
 app.get("/health", (req, res) => {
   res.send("Server is running fine");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
